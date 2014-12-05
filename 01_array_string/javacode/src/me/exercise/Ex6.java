66a8d3b3e98a4e2c027d711d18a41adf55dc718c
chase <chase@local>
2014-12-05 15:04:26 +0800

    ex1.6

--- 01_array_string/javacode/src/me/exercise/Ex6.java
+++ 01_array_string/javacode/src/me/exercise/Ex6.java
@@ -6,8 +6,59 @@ package me.exercise;
 public class Ex6 {
     // rotate matrix
 
-    int[][] makeMatrix(int w, int h) {
-        int[][] matrix = new int[w][h];
+    public int[][] makeMatrix(int size) {
+        int[][] matrix = new int[size][size];
+        int num = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = num;
+                num++;
+            }
+        }
+        return matrix;
+    }
 
+    public int[][] makeRotatedMatrix(int size) {
+        int[][] matrix = new int[size][size];
+        int num = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[j][size-i-1] = num;
+                num++;
+            }
+        }
+        return matrix;
+    }
+
+    public boolean matrixEquals(int[][] mat1, int[][] mat2) {
+        int size1 = mat1.length;
+        int size2 = mat2.length;
+
+        if (size1 != size2) return false;
+
+        int size = size1;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (mat1[i][j] != mat2[i][j]) return false;
+            }
+        }
+        return true;
+    }
+
+    private void swapElements(int[][] mat, int x1, int y1, int x2, int y2) {
+        int temp = mat[x1][y1];
+        mat[x1][y1] = mat[x2][y2];
+        mat[x2][y2] = temp;
+    }
+
+    public void rotateMatrix(int[][] mat) {
+        int size = mat.length;
+        for (int i = 0; i < size / 2; i++) {
+            for (int j = i; j < size-i-1; j++) {
+                swapElements(mat, i, j, size-j-1, i);
+                swapElements(mat, size-j-1, i, size-i-1, size-j-1);
+                swapElements(mat, size-i-1, size-j-1, j, size-i-1);
+            }
+        }
     }
 }
---
+++ 01_array_string/javacode/tests/me/exercise/Ex6Test.java
@@ -0,0 +1,42 @@
+package me.exercise;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Ex6Test {
+
+    private Ex6 ex6Instance;
+
+    private void outputMatrix(int[][] mat) {
+        int size = mat.length;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.format("%5d", mat[i][j]);
+            }
+            System.out.print('\n');
+        }
+        System.out.print('\n');
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        ex6Instance = new Ex6();
+    }
+
+    @Test
+    public void testRotateMatrix() throws Exception {
+        // even size matrix
+        int[][] mat = ex6Instance.makeMatrix(10);
+        int[][] matRotated = ex6Instance.makeRotatedMatrix(10);
+        ex6Instance.rotateMatrix(mat);
+
+        assertTrue("Rotated mat should equals matRotated", ex6Instance.matrixEquals(mat, matRotated));
+
+        mat = ex6Instance.makeMatrix(11);
+        matRotated = ex6Instance.makeRotatedMatrix(11);
+        ex6Instance.rotateMatrix(mat);
+        assertTrue("Rotated mat should equals matRotated", ex6Instance.matrixEquals(mat, matRotated));
+    }
+}
